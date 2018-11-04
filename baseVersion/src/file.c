f236fe3a6f18a391bd717665ab252dea5fe6fc53
Domenger <domenger@MacBook-Pro-de-Domenger.local>
2018-11-04 23:37:05 +0100

    reparer erreur

--- baseVersion/src/file.c
+++ baseVersion/src/file.c
@@ -3,3 +3,14 @@
 
 #include "file.h"
 
+struct cell {
+  char word[MAX_WORD_LENGTH+1];  // a char array for words of length MAX_WORD_LENGTH
+  int  noccs;                    // an integer for counting occurrences
+  struct cell* next;             // a (possibly NULL) pointer to the following cell
+};
+
+struct pool {
+  struct cell m[MAX_CELLS];      // an array of memory cells
+  struct cell* next_free;        // a pointer to the first free cell
+};
+struct pool pool = { {}, pool.m }; // Trick to initialize the pool statically
